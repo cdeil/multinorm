Fix precision matrix inverse implementation

multinorm.py
@@ -13,7 +13,7 @@ from pkg_resources import get_distribution, DistributionNotFound
 import functools
 import numpy as np
 import pandas as pd
-from scipy.linalg import pinvh, eigh
+from scipy.linalg import eigh
 from scipy.stats import multivariate_normal
 
 __all__ = ["MultiNorm"]
@@ -40,6 +40,13 @@ def cached_property(fn):
         return getattr(self, attr_name)
 
     return _cached_property
+
+
+def _matrix_inverse(matrix):
+    # np.linalg.inv seems to give numerically stable results
+    # We need inverse in several places, so in case there's
+    # a better option, and to get consistency, we put this wrapper function
+    return np.linalg.inv(matrix)
 
 
 class MultiNorm:
@@ -107,6 +114,12 @@ class MultiNorm:
         names : list
             Parameter names
         """
+        if err is None:
+            if mean is None:
+                raise ValueError('Must give mean or err')
+
+            err = np.ones_like(mean)
+
         err = np.asarray(err, dtype=float)
         n = len(err)
 
@@ -161,7 +174,7 @@ class MultiNorm:
 
         precisions = [_.precision.values for _ in distributions]
         precision = np.sum(precisions, axis=0)
-        cov = pinvh(precision)
+        cov = _matrix_inverse(precision)
 
         means_weighted = [_._mean_weighted for _ in distributions]
         means_weighted = np.sum(means_weighted, axis=0)
@@ -334,7 +347,7 @@ class MultiNorm:
 
         Sometimes called the "information matrix" or "Hesse matrix".
         """
-        matrix = self.scipy.cov_info.pinv
+        matrix = _matrix_inverse(self.scipy.cov)
         return self._pandas_matrix(matrix)
 
     def marginal(self, pars):
@@ -427,7 +440,7 @@ class MultiNorm:
 
         mean = self.scipy.mean[mask]
         precision = self.scipy.cov_info.pinv[np.ix_(mask, mask)]
-        cov = pinvh(precision)
+        cov = _matrix_inverse(precision)
         return self.__class__(mean, cov, names)
 
     def sigma_distance(self, point):

test_multinorm.py
@@ -45,6 +45,20 @@ def mn2():
     return MultiNorm(mean, cov, names)
 
 
+@pytest.fixture()
+def mn3():
+    """Example test case with very large and small numbers.
+
+    This is numerically challenging for several computations.
+    Ideally all methods in `MultiNorm` should be able to handle
+    this case and give accurate results, because it's quite common.
+    """
+    mean = np.array([1e-10, 1, 1e10])
+    err = 1.0 * mean
+    names = ['a', "b", "c"]
+    return MultiNorm.from_err(mean, err, names=names)
+
+
 def test_init():
     mn = MultiNorm(mean=[1, 2])
     assert mn.names == ["par_0", "par_1"]
@@ -114,6 +128,14 @@ def test_from_product():
     assert_allclose(mn.cov, [[0.5, 0], [0, 0.5]])
 
 
+def test_from_product_numerics(mn3):
+    # Check that `from_product` gives good results
+    # even if some parameters are very small and others very large
+    # Product of 10 measurements should reduce variance by factor 10
+    mn = MultiNorm.from_product([mn3] * 10)
+    assert_allclose(mn.cov, np.diag([1e-21, 0.1, 1e19]))
+
+
 def test_make_example():
     mn = MultiNorm.make_example(n_par=2, n_fix=1, random_state=0)
     assert_allclose(mn.mean, [1.76405235, 0.40015721, 0.97873798])
@@ -155,7 +177,7 @@ def test_correlation(mn1, mn2):
     assert_allclose(c.iloc[1, 2], 0.12909944)
 
 
-def test_precision(mn1, mn2):
+def test_precision(mn1, mn2, mn3):
     expected = np.diag([1, 1 / 4, 1 / 9])
     assert_allclose(mn1.precision, expected)
 
@@ -165,6 +187,9 @@ def test_precision(mn1, mn2):
         [0.36363636, -0.18181818, 0.36363636],
     ]
     assert_allclose(mn2.precision, expected)
+
+    expected = np.diag([1e20, 1, 1e-20])
+    assert_allclose(mn3.precision, expected)
 
 
 def test_marginal(mn1, mn2):